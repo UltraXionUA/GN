2a00c0513f07b7501aee242d2ea080306a135d16
UltraXionUA <ultra25813@gmail.com>
2020-03-22 14:21:31 +0200

    First_commit_to_new_repository

--- GNSite/MainApp/urls.py
+++ GNSite/MainApp/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index' )
+    path('', views.index, name='index')
 ]
 
--- config.py
+++ config.py
@@ -4,7 +4,6 @@ import pymysql.cursors
 TOKEN = '1077848786:AAHfMrRKadc3Plo14rpE7dPJJC3bVbbVod0'  # Release bot TOKEN
 TEST_TOKEN = '839168325:AAGtBUzQoqdPSSHPWUeo4K9Onxxnclv96sA'  # Test bot TOKE
 
-
 API = {'API_Weather': 'http://' + 'api.openweathermap.org/data/2.5/weather?q={}&appid=579cbfee514bcc3d3bec44b7ef405340'
                                   '&units=metric&leng=ru',
        'API_Gif': 'https://' + 'api.giphy.com/v1/gifs/random?api_key=mHVzi8lTLfPjh2qIAoXcN8P8QLeVRlvh&'\
