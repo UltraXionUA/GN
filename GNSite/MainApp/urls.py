2291dab065e76e695bbddd51e58b607ac9d46b36
UltraXionUA <ultra25813@gmail.com>
2020-03-22 15:38:16 +0200

    First_commit_to_new_repository

--- GNSite/MainApp/urls.py
+++ GNSite/MainApp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
-
+from django.http import request
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('/update_server', views.webhook, kwargs=['POST'])
 ]
 
--- GNSite/MainApp/views.py
+++ GNSite/MainApp/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+import git
 
 
 def index(request):
     return render(request, 'MainApp/index.html')
+
+
+def webhook(request):
+    if request.method == 'POST':
+        repo = git.Repo('https://github.com/UltraXionUA/GN')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updatpip ed PythonAnywhere successfully', 200
