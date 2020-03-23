66bed6e04a6b15f22f4851f9d8c8230568dfecbe
UltraXionUA <ultra25813@gmail.com>
2020-03-24 01:14:40 +0200

    testgithook

--- flaskmain.py
+++ flaskmain.py
@@ -24,78 +24,78 @@ def index():
     return '<h1>Hello World Bitch!!!<h1>'
 
 
-# @app.route('/update_server', methods=['POST'])
-# def webhook():
-#     if request.method != 'POST':
-#         return 'OK'
-#     else:
-#         abort_code = 418
-#         # Do initial validations on required headers
-#         if 'X-Github-Event' not in request.headers:
-#             abort(abort_code)
-#         if 'X-Github-Delivery' not in request.headers:
-#             abort(abort_code)
-#         if 'X-Hub-Signature' not in request.headers:
-#             abort(abort_code)
-#         if not request.is_json:
-#             abort(abort_code)
-#         if 'User-Agent' not in request.headers:
-#             abort(abort_code)
-#         ua = request.headers.get('User-Agent')
-#         if not ua.startswith('GitHub-Hookshot/'):
-#             abort(abort_code)
-#
-#         event = request.headers.get('X-GitHub-Event')
-#         if event == "ping":
-#             return json.dumps({'msg': 'Hi!'})
-#         if event != "push":
-#             return json.dumps({'msg': "Wrong event type"})
-#
-#         x_hub_signature = request.headers.get('X-Hub-Signature')
-#         # webhook content type should be application/json for request.data to have the payload
-#         # request.data is empty in case of x-www-form-urlencoded
-#         if not is_valid_signature(x_hub_signature, request.data, SECRET_KEY):
-#             print('Deploy signature failed: {sig}'.format(sig=x_hub_signature))
-#             abort(abort_code)
-#
-#         payload = request.get_json()
-#         if payload is None:
-#             print('Deploy payload is empty: {payload}'.format(
-#                 payload=payload))
-#             abort(abort_code)
-#
-#         if payload['ref'] != 'refs/heads/master':
-#             return json.dumps({'msg': 'Not master; ignoring'})
-#
-#         repo = git.Repo('/var/www/sites/mysite')
-#         origin = repo.remotes.origin
-#
-#         pull_info = origin.pull()
-#
-#         if len(pull_info) == 0:
-#             return json.dumps({'msg': "Didn't pull any information from remote!"})
-#         if pull_info[0].flags > 128:
-#             return json.dumps({'msg': "Didn't pull any information from remote!"})
-#
-#         commit_hash = pull_info[0].commit.hexsha
-#         build_commit = f'build_commit = "{commit_hash}"'
-#         print(f'{build_commit}')
-#         return 'Updated PythonAnywhere server to commit {commit}'.format(commit=commit_hash)
-
 @app.route('/update_server', methods=['POST'])
 def webhook():
-    x_hub_signature = request.headers.get('X-Hub-Signature')
-    if not is_valid_signature(x_hub_signature, request.data, SECRET_KEY):
-        if request.method == 'POST':
-            repo = git.Repo('/home/UltraXionUA/GN')
-            repo.commit()
-            origin = repo.remotes.origin
-            origin.pull()
-            return 'Updated PythonAnywhere successfully', 200
-        else:
-            return 'Wrong event type', 400
+    if request.method != 'POST':
+        return 'OK'
     else:
-        return 'Wrong SECRET_KEY'
+        abort_code = 418
+        # Do initial validations on required headers
+        if 'X-Github-Event' not in request.headers:
+            abort(abort_code)
+        if 'X-Github-Delivery' not in request.headers:
+            abort(abort_code)
+        if 'X-Hub-Signature' not in request.headers:
+            abort(abort_code)
+        if not request.is_json:
+            abort(abort_code)
+        if 'User-Agent' not in request.headers:
+            abort(abort_code)
+        ua = request.headers.get('User-Agent')
+        if not ua.startswith('GitHub-Hookshot/'):
+            abort(abort_code)
+
+        event = request.headers.get('X-GitHub-Event')
+        if event == "ping":
+            return json.dumps({'msg': 'Hi!'})
+        if event != "push":
+            return json.dumps({'msg': "Wrong event type"})
+
+        x_hub_signature = request.headers.get('X-Hub-Signature')
+        # webhook content type should be application/json for request.data to have the payload
+        # request.data is empty in case of x-www-form-urlencoded
+        if not is_valid_signature(x_hub_signature, request.data, SECRET_KEY):
+            print('Deploy signature failed: {sig}'.format(sig=x_hub_signature))
+            abort(abort_code)
+
+        payload = request.get_json()
+        if payload is None:
+            print('Deploy payload is empty: {payload}'.format(
+                payload=payload))
+            abort(abort_code)
+
+        if payload['ref'] != 'refs/heads/master':
+            return json.dumps({'msg': 'Not master; ignoring'})
+
+        repo = git.Repo('/home/UltraXionUA/GN')
+        origin = repo.remotes.origin
+
+        pull_info = origin.pull()
+
+        if len(pull_info) == 0:
+            return json.dumps({'msg': "Didn't pull any information from remote!"})
+        if pull_info[0].flags > 128:
+            return json.dumps({'msg': "Didn't pull any information from remote!"})
+
+        commit_hash = pull_info[0].commit.hexsha
+        build_commit = f'build_commit = "{commit_hash}"'
+        print(f'{build_commit}')
+        return 'Updated PythonAnywhere server to commit {commit}'.format(commit=commit_hash)
+
+# @app.route('/update_server', methods=['POST'])
+# def webhook():
+#     x_hub_signature = request.headers.get('X-Hub-Signature')
+#     if not is_valid_signature(x_hub_signature, request.data, SECRET_KEY):
+#         if request.method == 'POST':
+#             repo = git.Repo('/home/UltraXionUA/GN')
+#             repo.commit()
+#             origin = repo.remotes.origin
+#             origin.pull()
+#             return 'Updated PythonAnywhere successfully', 200
+#         else:
+#             return 'Wrong event type', 400
+#     else:
+#         return 'Wrong SECRET_KEY'
 # test2
 
 
