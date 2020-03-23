e45bbddb28ef28382200f50ea27c45dfa30b80bb
UltraXionUA <ultra25813@gmail.com>
2020-03-23 16:17:29 +0200

    First_commit_to_new_repository

--- GNSite/MainApp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-from django.http import HttpRequest
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('update_server/', views.webhook, name='hook')
-]
-
---
+++ flaskmain.py
@@ -0,0 +1,29 @@
+from flask import Flask, request
+import git
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return '<h1>Hello World<h1>'
+
+
+@app.route('/update_server', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('path/to/git_repo')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
+
+
+def main():
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
