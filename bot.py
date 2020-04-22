64290d7f14798640e2bdb75214814f83ff5a38b5
UltraXionUA <ultra25813@gmail.com>
2020-04-22 17:07:49 +0300

    fix

--- bot.py
+++ bot.py
@@ -40,6 +40,8 @@ Parser.start()
 @bot.message_handler(commands=['start'])  # /start
 def start_handler(message: Message) -> None:
     log(message, 'info')
+    if message.chat.id == "-1001339129150":
+        print('yes')
     if message.chat.type == 'private':
         db.add_user(message.from_user)
     else:
