b2ff40c8bc4338937328d65ab53a5c0593adf3b3
UltraXionUA <ultra25813@gmail.com>
2020-05-11 22:42:52 +0300

    fix

--- bot.py
+++ bot.py
@@ -2079,7 +2079,7 @@ def text_handler(message: Message) -> None:
             joke_handler(message)
         elif text in ['кубик', 'зарик', 'кость', 'хуюбик', 'dice']:
             dice_handler(message)
-        elif text in ['хентай', 'hentai', 'лоли', 'loli', 'девушка', 'girl', 'баба']:
+        elif text in ['хентай', 'hentai', 'лоли', 'loli', 'девушка', 'girl', 'баба', 'пизда']:
             forbidden_handler(message)
         if message.chat.type != 'private' and str(message.from_user.id) != GNBot_ID:
             if message.chat.id not in data_answers or len(data_answers[message.chat.id]) == 1:
