678db112da0c2946bc94a22c3f1589cc17777c98
UltraXionUA <ultra25813@gmail.com>
2020-05-13 13:44:18 +0300

    fix

--- db.py
+++ db.py
@@ -236,19 +236,70 @@ def get_task_answer(id_: str) -> str:
         result = cursor.fetchone()
     return result['answer']
 
+data = """[url=https://ibb.co/QvwWX1K][img]https://i.ibb.co/5hHQ2fR/51.png[/img][/url]
+[url=https://ibb.co/NZsRBNZ][img]https://i.ibb.co/kQxZCDQ/52.png[/img][/url]
+[url=https://ibb.co/BtTNVpS][img]https://i.ibb.co/G0CTv6b/53.png[/img][/url]
+[url=https://ibb.co/hgR6sJw][img]https://i.ibb.co/nwsGrNt/54.png[/img][/url]
+[url=https://ibb.co/YQ5gPHZ][img]https://i.ibb.co/zx3dQqJ/55.png[/img][/url]
+[url=https://ibb.co/sy3MB26][img]https://i.ibb.co/WgKYT6P/56.png[/img][/url]
+[url=https://ibb.co/ChRt41k][img]https://i.ibb.co/J5Dd8Bf/57.png[/img][/url]
+[url=https://ibb.co/WtX4G2N][img]https://i.ibb.co/JQTwmsW/58.png[/img][/url]
+[url=https://ibb.co/pzs0Sqq][img]https://i.ibb.co/wWb0ftt/59.png[/img][/url]
+[url=https://ibb.co/QQSz5Gn][img]https://i.ibb.co/sRxkBrQ/60.png[/img][/url]
+[url=https://ibb.co/bN5G5hS][img]https://i.ibb.co/kq2P2nR/61.png[/img][/url]
+[url=https://ibb.co/WKGkK0D][img]https://i.ibb.co/x5X15Ym/62.png[/img][/url]
+[url=https://ibb.co/j3qVpTw][img]https://i.ibb.co/WB7DT6z/63.png[/img][/url]
+[url=https://ibb.co/Mh97ScV][img]https://i.ibb.co/xs36L1j/64.png[/img][/url]
+[url=https://ibb.co/VqDLsv5][img]https://i.ibb.co/DRk1Z7T/65.png[/img][/url]
+[url=https://ibb.co/pQHbvVp][img]https://i.ibb.co/PM2FZX8/66.png[/img][/url]
+[url=https://ibb.co/WcKKQsK][img]https://i.ibb.co/p3XXgnX/67.png[/img][/url]
+[url=https://ibb.co/94ryrd5][img]https://i.ibb.co/z24P4tv/68.png[/img][/url]
+[url=https://ibb.co/ryQ8hjL][img]https://i.ibb.co/6sy9SMp/69.png[/img][/url]
+[url=https://ibb.co/nLY7DK0][img]https://i.ibb.co/87frjQB/70.png[/img][/url]
+[url=https://ibb.co/2K4SpNn][img]https://i.ibb.co/pXFQky0/71.png[/img][/url]
+[url=https://ibb.co/YWfqGYp][img]https://i.ibb.co/26cfBmg/72.png[/img][/url]
+[url=https://ibb.co/bBg7Nzx][img]https://i.ibb.co/85DXP6J/73.png[/img][/url]
+[url=https://ibb.co/gSQY1bF][img]https://i.ibb.co/F5cpvkq/74.png[/img][/url]
+[url=https://ibb.co/R6p7Lds][img]https://i.ibb.co/yknVL7Z/75.png[/img][/url]
+[url=https://ibb.co/LSfjtrM][img]https://i.ibb.co/7vFMWQH/76.png[/img][/url]
+[url=https://ibb.co/P17w0Bc][img]https://i.ibb.co/KVMFSC2/77.png[/img][/url]
+[url=https://ibb.co/8bdGM83][img]https://i.ibb.co/DY8m4KX/78.png[/img][/url]
+[url=https://ibb.co/V3tNBdr][img]https://i.ibb.co/7bXzyH8/79.png[/img][/url]
+[url=https://ibb.co/G3DzHY9][img]https://i.ibb.co/prTYhVQ/80.png[/img][/url]
+[url=https://ibb.co/MD01B6D][img]https://i.ibb.co/wNmQcsN/81.png[/img][/url]
+[url=https://ibb.co/nk1Zd6Q][img]https://i.ibb.co/2KMmVWc/82.png[/img][/url]
+[url=https://ibb.co/0FPwM62][img]https://i.ibb.co/SdqLnMy/83.png[/img][/url]
+[url=https://ibb.co/ZShWXBY][img]https://i.ibb.co/bFrzms2/84.png[/img][/url]
+[url=https://ibb.co/8shtxjN][img]https://i.ibb.co/yP37k8d/85.png[/img][/url]
+[url=https://ibb.co/4NTPZbL][img]https://i.ibb.co/F7wgbQ1/86.png[/img][/url]
+[url=https://ibb.co/LpByFmx][img]https://i.ibb.co/6sGSMzH/87.png[/img][/url]
+[url=https://ibb.co/mS0tHBN][img]https://i.ibb.co/Tr1RHTW/88.png[/img][/url]
+[url=https://ibb.co/JC2Hsnt][img]https://i.ibb.co/Yb8hL2Q/89.png[/img][/url]
+[url=https://ibb.co/3yGh198][img]https://i.ibb.co/LzbpSXq/90.png[/img][/url]
+[url=https://ibb.co/F83FT6H][img]https://i.ibb.co/DwWZ24r/91.png[/img][/url]
+[url=https://ibb.co/pP9w1d8][img]https://i.ibb.co/hMt9gVj/92.png[/img][/url]
+[url=https://ibb.co/2yR9kqj][img]https://i.ibb.co/vHfFj1J/93.png[/img][/url]
+[url=https://ibb.co/KNV8RMr][img]https://i.ibb.co/jV3jBcr/94.png[/img][/url]
+[url=https://ibb.co/8YDYBry][img]https://i.ibb.co/NT6T390/95.png[/img][/url]
+[url=https://ibb.co/3mWFyxX][img]https://i.ibb.co/0yZ9j0N/96.png[/img][/url]
+[url=https://ibb.co/qJ2zYtV][img]https://i.ibb.co/Hh90TW3/97.png[/img][/url]
+[url=https://ibb.co/WGng31f][img]https://i.ibb.co/n6rz0Sj/98.png[/img][/url]
+[url=https://ibb.co/2PrCg7d][img]https://i.ibb.co/S0gMRXB/99.png[/img][/url]
+[url=https://ibb.co/xjL4rnw][img]https://i.ibb.co/C78rSj4/100.png[/img][/url]"""
+def set_img():
+    import re
+    strings = data.split('\n')
+    connection = start_connection()
+    for en, i in enumerate(strings, 51):
+        link = re.search(r'https?://i.ibb.co/.+/\d+.\w+', i).group(0)
+        print(en, link)
+        with connection.cursor() as cursor:
+            cursor.execute(f'UPDATE Project_Euler SET image_url=\'{link}\' WHERE id={en};')
+        connection.commit()
+    connection.close()
 
-# def set_img():
-#     import re
-#     strings = data.split('\n')
-#     connection = start_connection()
-#     for en, i in enumerate(strings, 14):
-#         link = re.search(r'https?://i.ibb.co/.+/\d+.\w+', i).group(0)
-#         print(en, link)
-#         with connection.cursor() as cursor:
-#             cursor.execute(f'UPDATE Project_Euler SET image_url=\'{link}\' WHERE id={en};')
-#         connection.commit()
-#     connection.close()
 
+set_img()
 
 # def set_eluler(data: list) -> None:
 #     connection = start_connection()
